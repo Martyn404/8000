f2058e1f1971889e65e0ad9f0d0de7fc7eb0d007
Martyn <104262835+Martyn404@users.noreply.github.com>
2022-09-18 22:08:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+    var email = document.forms['form']['email'];
+    var password = document.forms['form']['password'];
+    
+    var error = document.getElementById('alert');  
+
+
+    
+    function validated(){
+        if (email.value == "Martyn" & password.value == "1029") {
+            error.style.display = "none";
+            return true;
+        }
+        else{
+            error.style.display = "block";
+            return false;
+        }
+    }
+    
+    
